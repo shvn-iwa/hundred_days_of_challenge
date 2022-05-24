5dd533fbc0db596c652a6ed66e914b1ddb93d583
shvn-iwa <73055963+shvn-iwa@users.noreply.github.com>
2022-05-24 23:57:10 +0900

    rewrite myapp.

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
